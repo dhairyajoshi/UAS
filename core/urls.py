a9b254a2e2d8e65c4840ac7e53b28ed2ab72d1ff
Priyanshu Kumar Singh <priyanshusingh1998@gmail.com>
2021-11-23 19:40:58 +0530

    added the serializers and urls

---
+++ core/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path, include
+from . import views as staff_views
+
+urlpatterns = [
+    # path('student-list', employee_views.StudentlList.as_view(), name="student-list"),
+
+]
