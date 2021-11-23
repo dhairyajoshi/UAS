2627779207c5cb49a1597836c1180697f0c3262f
Rittik Biswal <rittikbiswal2000@gmail.com>
2021-11-23 20:38:27 +0530

    Added faculty directory

--- faculty/models.py
+++ faculty/models.py
@@ -1,3 +1,15 @@
-from django.db import models
+from django.db import models 
+
+from django.contrib.auth.models import User
+
+class Faculty(models.Model):
+    user=models.ForeignKey(User,on_delete=models.CASCADE)
+    faculty_branch=models.CharField(max_length=50)
+    faculty_designation=models.CharField(max_length=50)
+    faculty_quaification=models.CharField(max_length=50)
+    faculty_specialization=models.CharField(max_length=50)
+    faculty_phone_number=models.CharField(max_length=50)
+    date_of_joining=models.CharField(max_length=50)
 
 # Create your models here.
+
