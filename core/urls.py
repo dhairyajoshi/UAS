73e597826c00ba9f2383dc675146a0f0c8f4612e
Priyanshu Kumar Singh <priyanshusingh1998@gmail.com>
2021-11-23 21:06:04 +0530

    added the user profile view

--- core/urls.py
+++ core/urls.py
@@ -3,4 +3,5 @@ from . import views as core_views
 
 urlpatterns = [
     path('branch-list', core_views.BranchList.as_view(), name="branch-list"),
+    path('user-profile', core_views.UserProfileView.as_view(), name="user-profile"),
 ]
--- core/views.py
+++ core/views.py
@@ -38,3 +38,69 @@ class CorouselDetail(generics.RetrieveDestroyAPIView):
     queryset = core_models.Branch.objects.all()
     serializer_class = core_serializers.BranchSerializer
 
+
+
+class UserProfileView(APIView):
+    permission_classes = [IsAuthenticated]
+    def get(self, request, *args, **kwargs):
+        site = request.META['HTTP_ORIGIN']
+        context = {}
+        curr_user = request.user
+        context['curr_user'] = core_serializers.UserSerializer(curr_user).data
+        # # if site == 'https://authors.kanzulhaya.com' or site=='http://localhost:3000':
+        #     # update_subscription()
+        # author_qs = core_models.Writer.objects.filter(user=request.user)
+        # if author_qs.exists():
+        #     context['curr_author'] = core_serializers.WriterSerializer(author_qs[0]).data
+        
+        # all_articles = request.query_params.get('all_author_articles')
+        # if all_articles:
+        #     context['all_articles'] = core_serializers.ArticleSerializer(core_models.Article.objects.filter(writer=author_qs[0]), many=True).data
+        
+        # # elif site == 'https://kanzulhaya.com' or site=='http://localhost:3000':
+        # user_active_plan = core_models.Subscription.objects.filter(user=curr_user, plan_active=True)
+        # user_all_plans = core_models.Subscription.objects.filter(user=curr_user, plan_active=True)
+        # user_free_trail = core_models.Subscription.objects.filter(user=curr_user, plan_id=1)
+
+        # if user_active_plan.exists():
+        #     context['user_active_plan'] = core_serializers.SubscriptionSerializer(user_active_plan[0]).data
+
+        # context['user_all_plans'] = core_serializers.SubscriptionSerializer(user_all_plans, many=True).data
+        # if user_free_trail.exists():
+        #     context['user_free_trail'] = core_serializers.SubscriptionSerializer(user_free_trail[0]).data
+
+        return Response(context, status=HTTP_200_OK)
+    
+    def post(self, request, *args, **kwargs):
+        context = {}
+        username = request.data.get('username')
+        first_name = request.data.get('first_name')
+        last_name = request.data.get('last_name')
+        email = request.data.get('email')
+        curr_user = request.user
+        if username:
+            curr_user.username = username
+        if first_name:
+            curr_user.first_name = first_name
+        if last_name:
+            curr_user.last_name = last_name
+        if email:
+            curr_user.email = email
+        curr_user.save()
+        context['curr_user'] = core_serializers.UserSerializer(curr_user).data
+        # author_qs = core_models.Writer.objects.filter(user=request.user)
+        # if author_qs.exists():
+        #     curr_author = author_qs[0]
+        #     serializer = core_serializers.WriterSerializer(curr_author, data=request.data, partial=True)
+        #     if serializer.is_valid():
+        #         serializer.save()
+        #         context['curr_author'] = serializer.data
+        #         return Response(context, status=HTTP_200_OK)
+        #     else:
+        #         return Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
+
+
+        return Response(context, status=HTTP_200_OK)
+
+
+
