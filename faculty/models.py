531034c271471132ae929298c0535ef4ece56234
Rittik Biswal <rittikbiswal2000@gmail.com>
2021-11-24 17:46:00 +0530

    added the local changes

--- faculty/models.py
+++ faculty/models.py
@@ -4,11 +4,11 @@ from django.contrib.auth.models import User
 
 class Faculty(models.Model):
     user=models.ForeignKey(User,on_delete=models.CASCADE)
-    faculty_branch=models.CharField(max_length=50)
-    faculty_designation=models.CharField(max_length=50)
-    faculty_quaification=models.CharField(max_length=50)
-    faculty_specialization=models.CharField(max_length=50)
-    faculty_phone_number=models.CharField(max_length=50)
+    branch=models.CharField(max_length=50)
+    designation=models.CharField(max_length=50)
+    quaification=models.CharField(max_length=50)
+    specialization=models.CharField(max_length=50)
+    phone_number=models.CharField(max_length=50)
     date_of_joining=models.CharField(max_length=50)
 
 # Create your models here.
