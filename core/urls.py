e8b87ce23089a2786d82b23fa324496ce4840ea0
PKSingghh <priyanshu.singh@solytics-partners.com>
2021-12-08 20:33:31 +0530

    added the detail page

--- core/urls.py
+++ core/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from . import views as core_views
 
 urlpatterns = [
-    path('branch-list', core_views.BranchList.as_view(), name="branch-list"),
-    path('branch-detail', core_views.BranchlDetail.as_view(), name="branch-detail"),
+    path('branch-list/', core_views.BranchList.as_view(), name="branch-list"),
+    path('branch-detail/<int:pk>/', core_views.BranchlDetail.as_view(), name="branch-detail"),
     path('user-profile', core_views.UserProfileView.as_view(), name="user-profile"),
 ]
