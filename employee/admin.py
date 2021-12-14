7eeb070902cc6ee927b7034af74ffd0d1ea39d1e
Gourav Das <gourav2001das@gmail.com>
2021-12-14 13:21:50 +0530

    Updated the employee admin

--- employee/admin.py
+++ employee/admin.py
@@ -1,7 +1,9 @@
-from . models import Employee
+from . models import Employee,Promotion,Leave,LeaveType
 from django.contrib import admin
 
 
 admin.site.register(Employee)
-
+admin.site.register(Promotion)
+admin.site.register(Leave)
+admin.site.register(LeaveType)
 # Register your models here.
