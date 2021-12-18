a6269dbd191d1f59680c1d57e2f98ac24aaa7c50
Gourav Das <gourav2001das@gmail.com>
2021-12-18 16:41:22 +0530

    Created the student,student application database

--- student/models.py
+++ student/models.py
@@ -2,16 +2,11 @@ from django.db import models
 from django.conf import settings
 from django.db.models.deletion import SET_DEFAULT
 from django_countries.fields import CountryField
-from core.models import Department
+from core.models import Department,Program
 from autoslug import AutoSlugField
 from django.template.defaultfilters import slugify
 
 
-STATUS_CHOICES =(
-    ('Accept','Accepted'),
-    ('Reject','Not Accepted')
-
-)
 
 ENTRANCE_CHOICES = (
     ('JEE-MAIN', 'JEE-MAIN'),
@@ -20,6 +15,14 @@ ENTRANCE_CHOICES = (
 
 PROGRAMME_CHOICES = (
     ('B.Tech.','B.Tech.'),
+    ('B.Tech. DD.','B.Tech. + M.Tech.'),
+    ('M.Tech.','M.Tech.'),
+    ('B.Arch.','B.Arch.'),
+    ('MCA','MCA'),
+    ('M.Sc.','M.Sc'),
+    ('Int.M.Sc','Int.M.Sc'),
+    ('M.Phil.','M.Phil.'),
+    ('Ph.D.','Ph.D')
 )
 
 RELIGION_CHOICES = (
@@ -46,67 +49,73 @@ BLOOD_CHOICES = (
     ("O-","O-")
 )
 
-TFW_CHOICES = (
-    ("YES","YES"),
-    ("NO","NO")
-)
 
 MODE_CHOICES = (
     ("REGULAR","REGULAR"),
     ("SELF-SUSTAINING","SELF-SUSTAINING")
 )
 
-PWD_CHOICES = (
-    ("YES","YES"),
-    ("NO","NO")
-)
-
-DEFENCE_CHOICES = (
-    ("YES","YES"),
-    ("NO","NO")
-)
-
-GREENCARD_CHOICES = (
-    ("YES","YES"),
-    ("NO","NO")
-)
 
 class Student_Application(models.Model):
-    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE,null=True)
     dept = models.ForeignKey(Department, on_delete=models.CASCADE,null= True)
+    course = models.ManyToManyField(Program,related_name='student_applicant_courses')
     jee_roll = models.CharField(max_length=100, null=True)
     jee_rank = models.CharField(max_length=100, null=True)
     programme = models.CharField(max_length=50,choices=PROGRAMME_CHOICES, null=True)
     #academic_session = models.CharField(max_length=50,choices=(2021,)),
     entrance_exam = models.CharField(max_length=50,choices=ENTRANCE_CHOICES,default = "JEE-MAIN")
-    is_Tfw = models.CharField(max_length=5,choices = TFW_CHOICES,default = "NO")
+    is_Tfw = models.BooleanField(default = False)
     mode = models.CharField(max_length=50,choices=MODE_CHOICES,default= "REGULAR")
-    is_pwd = models.CharField(max_length = 5,choices = PWD_CHOICES,default="NO")
-    is_defence = models.CharField(max_length = 5,choices=DEFENCE_CHOICES,default="NO")
-    is_green_card = models.CharField(max_length = 5,choices = GREENCARD_CHOICES,default="NO")
+    is_pwd = models.BooleanField(default=False)
+    is_defence = models.BooleanField(default=False)
+    is_green_card = models.BooleanField(default=False)
     parents_mobile = models.CharField(max_length=12,null = True)
     parents_email = models.EmailField(null= True)
     application_time = models.DateTimeField(null = True)
-    status = models.CharField(max_length =10,choices=STATUS_CHOICES,default = 'Reject')
+    status = models.BooleanField(default=False)
+
+    slug = models.SlugField(unique= True,blank=True)
+    def __str__(self):
+        return self.user.first_name + ' ' + self.user.last_name
+
+    def save(self,*args,**kwargs):
+        self.slug = slugify(self.first_name + '-' + self.last_name)
+        slug_eixts = Student_Application.objects.filter(slug=self.slug).exists()
+        if slug_eixts:
+            self.slug += '-' + str(self.user.id)
+        
+        super(Student_Application, self).save(*args, **kwargs)
 
 
 class Student(models.Model):
-    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE),
-    application = models.OneToOneField('Student_Application', on_delete=models.CASCADE),
-    verification_time = models.DateTimeField()
-
-
-    #verify this
-    verified_by = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True)
-
-
-
-
-
-
-
+    registration =models.BigIntegerField(unique=True,null=True)
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE,null=True)
+    application = models.OneToOneField(Student_Application, on_delete=models.CASCADE,null =True)
+    verification_time = models.DateTimeField(null =True)
+    verified_by = models.ForeignKey(settings.AUTH_USER_MODEL,related_name='student_verifier', on_delete=models.SET_NULL, null=True)
+    dept = models.ForeignKey(Department,on_delete = models.CASCADE,null= True)
+    course = models.ForeignKey(Program,on_delete = models.CASCADE,null=True)
+    entrance_exam = models.CharField(max_length = 50,choices=ENTRANCE_CHOICES,default= "JEE-MAIN")
+    academic_session= models.CharField(max_length=50,null=True)
+    is_tfw = models.BooleanField(default=False)
+    mode = models.CharField(max_length=50,choices=MODE_CHOICES,default="REGULAR")
+    is_pwd = models.BooleanField(default=False)
+    is_defence = models.BooleanField(default=False)
+    is_green_card = models.BooleanField(default=False)
+    parent_email = models.EmailField(null= True)
+    parent_mobile = models.CharField(max_length=12,null=True)
+    jee_roll = models.CharField(max_length=50,null=True)
+    jee_rank = models.CharField(max_length=50,null=True)
+    entry_gate = models.CharField(max_length= 50,null=True)
+    program = models.CharField(max_length=50,choices = PROGRAMME_CHOICES,null= True)
+    application_time = models.DateTimeField(null= True)
+    status = models.BooleanField(default = False)
 
     slug = models.SlugField(unique=True, blank=True)
+    
+    class Meta:
+        unique_together = (("registration"),)
 
     def __str__(self):
         return self.user.first_name + ' ' + self.user.last_name
