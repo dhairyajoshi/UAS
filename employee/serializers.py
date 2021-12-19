0a9948b733f977f7eb1476051b23d508f86ca820
Soubhagya <soubhagyaranjanghs@gmail.com>
2021-12-19 22:28:10 +0530

    added faculty and employee serializers

--- employee/serializers.py
+++ employee/serializers.py
@@ -1,17 +1,16 @@
 from rest_framework import serializers
-from . import models as faculty_models
+from . import models as employee_models
 from django.contrib.auth.models import User
-
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         fields = ('username', 'email', 'first_name', 'last_name')
         model = User
 
-class FacultySerializer(serializers.ModelSerializer):
-    faculty = UserSerializer(read_only=False)
+
+class EmployeeSerializer(serializers.ModelSerializer):
+    employee = UserSerializer(read_only=False)
     class Meta:
         # fields = '__all__'
         exclude = ('password')
-        model = faculty_models.Faculty
-
+        model = employee_models.Employee
