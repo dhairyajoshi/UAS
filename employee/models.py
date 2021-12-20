210348305aa27093151b7a4a050d92814dc288f9
Gourav Das <gourav2001das@gmail.com>
2021-12-20 19:26:14 +0530

    Created the role table

--- employee/models.py
+++ employee/models.py
@@ -100,3 +100,17 @@ class Experience(models.Model):
 
     def __str__(self):
         return self.user.first_name + "_" + self.user.last_name
+
+class role(models.Model):
+    emp = models.ForeignKey(Employee,on_delete=models.CASCADE,related_name="employee_role",null=True)
+    name = models.CharField(max_length=50,null =True)
+    start_date = models.DateField(null =True)
+    end_date = models.DateField(null =True)
+    description = models.CharField(max_length=100,null =True)
+
+    def __str__(self):
+        return self.user.first_name+ "_" + self.name
+    
+    class Meta:
+        verbose_name_plural = "Roles"
+
