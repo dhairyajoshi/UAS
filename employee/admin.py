fc184b950ed1913cfdab89df032a368fdb639ece
Gourav Das <gourav2001das@gmail.com>
2021-12-20 22:38:03 +0530

    Registered the account_detail and role model in employee admin

--- employee/admin.py
+++ employee/admin.py
@@ -1,4 +1,4 @@
-from . models import Employee,Promotion,Leave,LeaveType,Designation,Experience
+from . models import Employee,Promotion,Leave,LeaveType,Designation,Experience,Account_Detail,role
 from django.contrib import admin
 
 
@@ -8,5 +8,7 @@ admin.site.register(Leave)
 admin.site.register(LeaveType)
 admin.site.register(Designation)
 admin.site.register(Experience)
+admin.site.register(Account_Detail)
+admin.site.register(role)
 
 # Register your models here.
