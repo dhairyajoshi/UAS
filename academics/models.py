7c50f6866ea0bd252b695da2e78eb8ecd99056ac
Priyanshu Kumar Singh <priyanshu.singh@solytics-partners.com>
2021-12-23 00:19:36 +0530

    made new migration files

--- academics/models.py
+++ academics/models.py
@@ -2,9 +2,9 @@ from django.db import models
 from django.db.models.base import Model
 from django.db.models.fields import related
 from django.utils import tree
-from core.models import Department
-from faculty.models import Faculty
-from student.models import Student
+from core import models as core_models
+from faculty import models as faculty_models
+from student import models as student_models
 # Create your models here.
 
 COURSE_TYPE_CHOICES=(
@@ -29,7 +29,7 @@ class Program(models.Model):
     num_of_seats = models.IntegerField()
     is_ugc_accre = models.BooleanField(default= False)
     is_nb_accre = models.BooleanField(default= False)
-    departments = models.ForeignKey(to= 'core.Department',on_delete=models.CASCADE,related_name="program_department",null=True)
+    departments = models.ManyToManyField(core_models.Department,related_name="program_department")
 
     def __str__(self):
         return self.full_name
@@ -43,7 +43,7 @@ class Course(models.Model):
     credit = models.IntegerField()
     contact_hours = models.IntegerField()
     course_type = models.CharField(choices = COURSE_TYPE_CHOICES,max_length=50,null = True)
-    dept = models.ForeignKey(to='core.Department',related_name='course_department',on_delete=models.CASCADE,null = True)
+    department = models.ForeignKey(core_models.Department,related_name='course_department',on_delete=models.CASCADE,null = True)
     def __str__(self):
         return self.course_code
     class Meta:
@@ -52,7 +52,7 @@ class Course(models.Model):
 class SemesterCourse(models.Model):
     sem_record = models.ForeignKey('SemesterRecord',on_delete = models.CASCADE, null =True)
     course = models.ForeignKey(Course,related_name='sem_coursedetails',on_delete=models.CASCADE,null = True)
-    faculty =models.ForeignKey(Faculty,related_name='semcourse_fcltydetails',on_delete=models.CASCADE,null = True)
+    faculty =models.ForeignKey(faculty_models.Faculty,related_name='semcourse_fcltydetails',on_delete=models.CASCADE,null = True)
     daily_class_report = models.ManyToManyField('DailyClassReport',related_name= 'sem_course_daily_class_report')
 
     class Meta:
@@ -63,7 +63,7 @@ class SemesterRecord(models.Model):
     student = models.ManyToManyField(to='student.Student',related_name="sem_rec_studentdetail")
     sem_course = models.ManyToManyField(SemesterCourse,related_name="sem_rec_sem_coursedetail")
     sem_type = models.CharField(max_length=10,choices=SEMESTER_CHOICES,null =True)
-    dept = models.ForeignKey(Department,on_delete=models.CASCADE,null = True)
+    dept = models.ForeignKey(core_models.Department,on_delete=models.CASCADE,null = True)
 
     def __str__(self):
         return self.sem_course.course.subject + "_" +self.start_date
@@ -76,16 +76,18 @@ class Examination(models.Model):
     sem_course = models.ForeignKey(SemesterCourse,related_name='exam_semcoursedetails',on_delete=models.CASCADE,null = True)
     total_marks = models.IntegerField()
     exam_type = models.CharField(max_length=50,choices=EXAMINATION_CHOICES,default = "ENDSEM")
-    dept = models.ForeignKey(Department,related_name= 'Exam_deptdetail',on_delete=models.CASCADE,null= True)
+    dept = models.ForeignKey(core_models.Department, related_name= 'Exam_deptdetail',on_delete=models.CASCADE,null= True)
 
     class Meta:
         verbose_name_plural = "Examinations"
+
+
 class DailyClassReport(models.Model):
     sem_course = models.ForeignKey(SemesterCourse,related_name='clsrpt_semcoursedetails',on_delete= models.CASCADE,null = True)
     class_date = models.DateField(null=True)
     class_time = models.TimeField(null= True)
-    fclt = models.ForeignKey(Faculty,related_name='clsrpt_fcltydetails',on_delete=models.CASCADE,null = True)
-    student_prsnt = models.ManyToManyField(to= 'student.Student',related_name="clsrpt_Studentdetail")
+    faculty = models.ForeignKey(faculty_models.Faculty,related_name='clsrpt_fcltydetails',on_delete=models.CASCADE,null = True)
+    student_prsnt = models.ManyToManyField(student_models.Student,related_name="clsrpt_Studentdetail")
     topics_cover = models.IntegerField()
 
     def __str__(self):
@@ -95,13 +97,13 @@ class DailyClassReport(models.Model):
         verbose_name_plural = 'DailyClassReports'
 
 class SemesterMarks(models.Model):
-    student = models.OneToOneField(Student,on_delete=models.SET_NULL,null=True)
+    student = models.OneToOneField(student_models.Student,on_delete=models.SET_NULL,null=True)
     sem_course = models.ForeignKey(SemesterCourse,on_delete=models.CASCADE,related_name='semmark_coursedetails',null = True)
     total_marks = models.IntegerField()
     marks_obtained = models.IntegerField()
     exam = models.OneToOneField(Examination,on_delete = models.CASCADE,null=True)
-    fclt = models.ForeignKey(Faculty,related_name='semmarks_fcltydetails',on_delete=models.CASCADE,null = True)
-    dept = models.ManyToManyField(Department,related_name="semmarks_deptdetails")
+    faculty = models.ForeignKey(faculty_models.Faculty,related_name='semmarks_fcltydetails',on_delete=models.CASCADE,null = True)
+    department = models.ForeignKey(core_models.Department,related_name="semmarks_deptdetails", on_delete=models.CASCADE)
 
     class Meta:
         verbose_name_plural= 'SemesterMarks'
