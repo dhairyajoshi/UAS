e405f423d581101895a9017a7c9f7c78ed85e425
Gourav Das <gourav2001das@gmail.com>
2022-01-16 20:57:35 +0530

    Changed the staff registration endpoint

--- staff/urls.py
+++ staff/urls.py
@@ -2,5 +2,5 @@ from django.urls import path, include
 from . import views as staff_views
 
 urlpatterns = [
-    path('staff-registration/',staff_views.StaffRegistration.as_view(),name="Staff-registration")
+    path('staff-list/',staff_views.StaffRegistration.as_view(),name="Staff-registration")
 ]
